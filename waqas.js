64f272d769c5d1b9d27800612d0f9aa11a09d7c0
muhammadwaqas325 <muhammadwaqaskhan4971@gmail.com>
2024-08-21 12:10:17 +0500

    Add files via upload

---
+++ waqas.js
@@ -0,0 +1,141 @@
+
+// ---------------------------------Alerts- 1-------------------------------------------------------------------
+ // alert("waqas senga chal da")
+ // console.log("waqas senga chal da")
+ // document.write("waqas senga chal da")
+ // console.warn("waqas senga chal da")
+
+// ---------------------------------------------------------------------------------------------------------------------------
+                            // variable   2
+
+ // let firstName = " Muhammad"
+ // let lastName = " Waqas"
+ // console.log("student name" + firstName + lastName)
+
+// ----------------------------------------------------------------------------------------------------------
+
+                            //  variable   3
+
+  // let age = 18;
+  // console.log("Age :" +age)    
+  
+   let rs = 100;
+   rs =200;
+
+   constprice = 25
+   price =25
+   console.log(price)
+
+  console.log("mango price :" + rs)
+
+//-----------------------------------------------------------------------------------------------------------------------------
+                           //variable Name legal and illegal 4
+//------------------------------------------------------------------------------------------
+                 //maths expration familiar operatos 5
+                 //+;-;*;/;%
+  console.log( 3 + 4)                                         //7
+  console.log( 5 - 9)                                        // -4 
+  console.log( 6 * 3)                                       // 18  
+  console.log( 20 / 5)                                     // 4
+  console.log( 16 % 3)                                    // 1
+  console.log( 2 + 3 - 8 * 2 / 4 +3 -9)                  //-5
+  console.log( 2 * 2 * 5 -5 / 2 + 15)                   //32.5
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+                  //maths exprassion unfamiliar operate 6
+  //num++;   num--;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+//     ifstatments
+//     let age =18
+//     if (age > 17)
+//let age = prompt("enter your age ")
+//let lic = prompt("do you have lic =yes")
+///if (age <=18){
+   // console.log("you can drive")
+//}
+//else {
+  //  console.log("try next time")
+//}
+//let age = prompt("please inter age")
+//if (age > 60 || age <18){
+  //  console.log(50
+//}
+//let ovieNight =prompt (" you finshed your homework")
+//let MovieNight=prompt("you can not finshed your homework")
+//if ("you complete your homework || you can not complete homework"){
+ 
+  //  console.log("yes")
+//}
+// let email ="waqas@gmail.com"
+// let pass = "323567"
+
+// if (email == "waqas@gmail.com"){
+
+//   if(pass == "323567")
+//   {
+  
+//     console.log("both correct")
+
+//   }
+//   else {
+//     console.log("pass is incorrect")
+//   }
+//      console.log("email is correct")
+//    }
+//    else{
+//     console.log(" both are not correct")
+//    }
+
+// ---------------------------------- Arrays ---------------------------------------------------------------------------
+
+
+// let cities = ["Peshawar" , "islamabad" , "Lahore" ]
+ 
+// console.log(cities [1])
+
+// let firstName ;
+// firstName ="khan"
+// console.log(firstName)
+
+
+// let student = [] ;
+
+// student.push("ali , waqas, wajid")
+
+// console.log(student)
+
+
+// let student = [] ;
+
+// student.push("ali" , "waqas" , "wajid" , "waseem")
+
+// student.pop()
+
+// console.log(student)
